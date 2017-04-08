dc9303328461622cd94f03ab9cfa890319327523
Edgar Briceno <bricenoe0911@gmail.com>
2017-04-08 15:28:53 -0500

    Actualizacion Proyecto Web

---
+++ ParteII/WebExamen/WebBancoSucursales/Controllers/BancoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBancoSucursales.Controllers
+{
+    public class BancoController : Controller
+    {
+        //
+        // GET: /Banco/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
